e8e230a50d6cab600cc3b90bed9a03ee3e06ab3e
Randy Powelson <randy.powelson@yahoo.com>
2022-12-21 12:34:07 -0800

    solved a few problems

--- problems/02-my-map.js
+++ problems/02-my-map.js
@@ -16,8 +16,14 @@ let result2 = myMap(['run', 'Forrest'], function (el) {
 console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 *******************************************************************************/
 
-let myMap = function() {
-
+let myMap = function(array, cb) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        let el = array[i];
+        let res = cb(el, i, array);
+        newArray.push(res);
+    }
+    return newArray;
 };
 
 
