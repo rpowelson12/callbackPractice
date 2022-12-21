b67d02c68d7894e0fafbc872c732c902840df187
Randy Powelson <randy.powelson@yahoo.com>
2022-12-21 12:25:46 -0800

    adding tests

--- problems/01-my-for-each.js
+++ problems/01-my-for-each.js
@@ -21,8 +21,11 @@ myForEach(['laika', 'belka'], function (el) {
 console.log(test); // ['LAIKA', 'BELKA']
 *******************************************************************************/
 
-let myForEach = function() {
-
+let myForEach = function (array, cb) {
+    for (let i = 0; i < array.length; i++) {
+        let el = array[i];
+        cb(el, i, array);
+    }
 };
 
 
